06554335fbf00aaa69161aabb877a2dd5bc32a46
tkooooo123 <tkooooo123@gmail.com>
2023-01-15 19:19:07 +0800

    串接登入與註冊之api

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,45 +1,28 @@
 <template>
   <div class="container py-5">
-   
-    <form class="w-40" 
-    @submit.prevent.stop="handleSubmit"
-    >
     <div class="title-wrapper text-center mb-4 d-flex justify-content-center">
-        <div
-          class="title-login"
-          :class="{ selected: loginSelected }"
-          @click.prevent.stop="handleSelect"
-        >
-          會員登入
-        </div>
-        <span class="line"></span>
-        <p
-          class="title-register"
-          :class="{ selected: registerSelected }"
-          @click.prevent.stop="handleSelect"
-        >
-          立即註冊
-        </p>
+      <div
+        class="title-login"
+        :class="{ selected: loginSelected }"
+        @click.prevent.stop="loginSelect"
+      >
+        會員登入
       </div>
+      <span class="line"></span>
+      <div
+        class="title-register"
+        :class="{ selected: registerSelected }"
+        @click.prevent.stop="registerSelect"
+      >
+        立即註冊
+      </div>
+    </div>
+    <form class="w-40" v-if="loginSelected" @submit.prevent.stop="loginSubmit">
       <div class="login-wrapper">
-        <div class="form-label-group mb-2"
-        v-if="registerSelected"
-        >
-          <label for="name" hidden>Name</label>
-          <input
-          v-model="name"
-            id="name"
-            name="name"
-            type="text"
-            class="form-control"
-            placeholder="請輸入名字"
-            required
-          />
-        </div>
         <div class="form-label-group mb-2">
           <label for="email" hidden>email</label>
           <input
-          v-model="email"
+            v-model="email"
             id="email"
             name="email"
             type="email"
@@ -51,7 +34,7 @@
         <div class="form-label-group mb-3">
           <label for="password" hidden>Password</label>
           <input
-          v-model="password"
+            v-model="password"
             id="password"
             name="password"
             type="password"
@@ -60,68 +43,173 @@
             required
           />
         </div>
-        <div class="form-label-group mb-3"
-        v-if="registerSelected">
-          <label for="passwordCheck" hidden>Password Check</label>
+        <button
+          class="btn btn-lg btn-primary btn-block mb-3"
+          type="submit"
+          v-if="loginSelected"
+        >
+          會員登入
+        </button>
+      </div>
+    </form>
+
+    <form
+      class="w-40"
+      v-if="registerSelected"
+      @submit.prevent.stop="registerSubmit"
+    >
+      <div class="login-wrapper">
+        <div class="form-label-group mb-2">
+          <label for="name" hidden>Name</label>
           <input
-          v-model="passwordCheck"
-            id="passwordCheck"
-            name="passwordCheck"
+            v-model="name"
+            id="name"
+            name="name"
+            type="text"
+            class="form-control"
+            placeholder="請輸入名字"
+            required
+          />
+        </div>
+        <div class="form-label-group mb-2">
+          <label for="email" hidden>email</label>
+          <input
+            v-model="email"
+            id="email"
+            name="email"
+            type="email"
+            class="form-control"
+            placeholder="請輸入 Email"
+            required
+          />
+        </div>
+        <div class="form-label-group mb-3">
+          <label for="password" hidden>Password</label>
+          <input
+            v-model="password"
+            id="password"
+            name="password"
+            type="password"
+            class="form-control"
+            placeholder="請輸入密碼"
+            required
+          />
+        </div>
+        <div class="form-label-group mb-3">
+          <label for="confirmPassword" hidden>Password Check</label>
+          <input
+            v-model="confirmPassword"
+            id="confirmPassword"
+            name="confirmPassword"
             type="password"
             class="form-control"
             placeholder="請再一次輸入密碼"
             required
           />
         </div>
-        <button class="btn btn-lg btn-primary btn-block mb-3" type="submit"
-        v-if="loginSelected"
-        >
-          會員登入
-        </button>
-        <button class="btn btn-lg btn-primary btn-block mb-3" type="submit"
-        v-if="registerSelected"
-        >
+        <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">
           立即註冊
         </button>
       </div>
     </form>
-    
-
   </div>
 </template>
 
 <script>
+import authorizationAPI from "../apis/authorization";
+import { Toast } from "./../utils/helpers";
+
 export default {
   data() {
     return {
-      name:'',
-      email:'',
-      password:'',
-      passwordCheck: '',
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
       loginSelected: true,
       registerSelected: false,
     };
   },
   methods: {
-    handleSubmit() {
-      const data = JSON.stringify({
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        passwordCheck:this.passwordCheck
-      });
-      console.log("data", data);
+    async loginSubmit() {
+      try {
+        if (!this.email || !this.password) {
+          Toast.fire({
+            icon: "warning",
+            title: "請填入所有欄位",
+          });
+          return;
+        }
+
+        const response = await authorizationAPI.signIn({
+          email: this.email,
+          password: this.password,
+        });
+        console.log("response", response);
+        const { data } = response;
+        if (data.status === "error") {
+          throw new Error(data.message);
+        }
+        localStorage.setItem("token", data.token);
+        this.$router.push("/products");
+      } catch (error) {
+        this.password = "";
+        Toast.fire({
+          icon: "warning",
+          title: error.message,
+        });
+        console.log(error);
+      }
     },
-    
-    handleSelect() {
-      if (this.loginSelected === true) {
+    async registerSubmit() {
+      try {
+        if (
+          !this.name ||
+          !this.email ||
+          !this.password ||
+          !this.confirmPassword
+        ) {
+          Toast.fire({
+            icon: "warning",
+            title: "請填入所有欄位",
+          });
+          return;
+        }
+
+        const response = await authorizationAPI.signUp({
+          name: this.name,
+          email: this.email,
+          password: this.password,
+          confirmPassword: this.confirmPassword,
+        });
+        console.log("response", response);
+        const { data } = response;
+        if (data.status === "error") {
+          throw new Error(data.message);
+        }
+
+        localStorage.setItem("token", data.token);
+        this.$router.go(0);
+      } catch (error) {
+        Toast.fire({
+          icon: "warning",
+          title: error.message,
+        });
+        console.log(error);
+      }
+    },
+
+    registerSelect() {
+      if (this.registerSelected === false) {
         this.loginSelected = false;
         this.registerSelected = true;
-      } else {
+      }
+    },
+    loginSelect() {
+      if (this.loginSelected === false) {
         this.loginSelected = true;
         this.registerSelected = false;
       }
-      console.log(this.isSelected);
     },
   },
 };
@@ -129,11 +217,15 @@ export default {
 
 <style lang="scss">
 .container {
-  display: flex;
+  display: block;
   justify-content: center;
-
   .title-wrapper {
+    margin: auto 20%;
     text-align: center;
+    @media (min-width: 900px) {
+      margin: auto 30%;
+    }
+
     .line {
       background: #747373;
       width: 1px;
@@ -145,14 +237,16 @@ export default {
       margin: auto;
       font-size: 1.5rem;
       font-weight: 550;
-      
     }
     .selected {
       color: red;
     }
   }
-  .form-wrapper {
-    
+  .login-wrapper {
+    margin: auto 20%;
+    @media (min-width: 900px) {
+      margin: auto 30%;
+    }
   }
 }
 </style>
